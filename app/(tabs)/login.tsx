16d229f0c2aa1da397f5bdcc24c0b53a604f36c5
ekduszla <dayeonkim0228@gmail.com>
2025-08-20 00:56:09 +0900

    폰트 적용

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,9 +1,33 @@
+import { useFonts } from 'expo-font';
 import { Text, View } from "react-native";
 
 export default function Login() {
+
+    const [fontsLoaded] = useFonts({
+        "NanumSquareNeo-Bd" : require('../../assets/fonts/NanumSquareNeo-Bd.ttf'),
+        "NanumSquareNeo-Eb" : require('../../assets/fonts/NanumSquareNeo-Eb.ttf'),
+        "NanumSquareNeo-Hv" : require('../../assets/fonts/NanumSquareNeo-Hv.ttf'),
+        "NanumSquareNeo-Lt" : require('../../assets/fonts/NanumSquareNeo-Lt.ttf'),
+        "NanumSquareNeo-Rg" : require('../../assets/fonts/NanumSquareNeo-Rg.ttf'),
+
+        "Pretendard-Black" : require('../../assets/fonts/Pretendard-Black.ttf'),
+        "Pretendard-Bold" : require('../../assets/fonts/Pretendard-Bold.ttf'),
+        "Pretendard-ExtraBold" : require('../../assets/fonts/Pretendard-ExtraBold.ttf'),
+        "Pretendard-ExtraLight" : require('../../assets/fonts/Pretendard-ExtraLight.ttf'),
+        "Pretendard-Light" : require('../../assets/fonts/Pretendard-Light.ttf'),
+        "Pretendard-Medium" : require('../../assets/fonts/Pretendard-Medium.ttf'),
+        "Pretendard-Regular" : require('../../assets/fonts/Pretendard-Regular.ttf'),
+        "Pretendard-SemiBold" : require('../../assets/fonts/Pretendard-SemiBold.ttf'),
+        "Pretendard-Thin" : require('../../assets/fonts/Pretendard-Thin.ttf'),
+    });
+
+    if (!fontsLoaded) return null;
+
     return (
         <View style={{flex : 1, justifyContent : "center", alignItems : "center"}}>
-            <Text> Login </Text>
+            <Text style={{fontSize:20}}> default pretendard nanumsqureneo</Text>
+            <Text style ={{fontFamily :"Pretendard-Regular", fontSize: 20}}> default pretendard nanumsqureneo </Text>
+            <Text style ={{fontFamily :"NanumSquareNeo-Rg", fontSize: 20}}> default pretendard nanumsqureneo </Text>  
         </View>
     );
 }

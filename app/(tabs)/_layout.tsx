c0e4b6577073664504eb76db725e4f11991baf11
ekduszla <dayeonkim0228@gmail.com>
2025-08-30 23:33:50 +0900

    정상화

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Tabs } from "expo-router";
+
+export default function TabLayout(){
+    return (
+        <Tabs screenOptions={{headerShown : false}}>
+            <Tabs.Screen name="home"/>
+            <Tabs.Screen name="login"/>
+            <Tabs.Screen name="signup"/>
+        </Tabs>
+    );
+}

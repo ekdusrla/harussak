163e474834a024dafbb2ade03ed4c654fc8e8c27
ekduszla <dayeonkim0228@gmail.com>
2025-09-16 15:19:15 +0900

    오류 수정

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,62 @@
 import { Tabs } from "expo-router";
+import { Image } from "react-native";
 
-export default function TabLayout(){
-    return (
-        <Tabs screenOptions={{headerShown : false}}>
-            <Tabs.Screen name="home"/>
-            <Tabs.Screen name="login"/>
-            <Tabs.Screen name="routine"/>
-        </Tabs>
-    );
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false, // 라벨 숨기고 아이콘만
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={
+                focused
+                  ? require("../../assets/images/icon-homeN.png")
+                  : require("../../assets/images/icon-homeN.png")
+              }
+              style={{ width: 34, height: 34, marginBottom: -16 }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="login"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={
+                focused
+                  ? require("../../assets/images/icon-homeN.png")
+                  : require("../../assets/images/icon-homeN.png")
+              }
+              style={{ width: 40, height: 40 }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="routine"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={
+                focused
+                  ? require("../../assets/images/icon-homeN.png")
+                  : require("../../assets/images/icon-homeN.png")
+              }
+              style={{ width: 40, height: 40 }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
